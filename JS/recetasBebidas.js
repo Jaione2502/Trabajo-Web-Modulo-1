a4ba6e20b11efb39e38e0bd0e298e7a9a986eef2
InesFr <ines23franco@gmail.com>
2025-03-11 19:49:47 +0100

    Añadir bebidas y otras actualizaciones

---
+++ JS/recetasBebidas.js
@@ -0,0 +1,30 @@
+const apiKey = '14a96f3eeb7242fdb189b3560d91cd49'; 
+const apiUrl = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&type=drink&number=6`;
+
+document.addEventListener("DOMContentLoaded", pedir);
+
+function pedir() {
+  fetch(apiUrl)
+    .then(res => res.json())
+    .then(data => mostrarBebidas(data.results)) 
+    .catch(error => console.error("Error al obtener las bebidas:", error));
+}
+
+function mostrarBebidas(bebidas) {
+  const container = document.getElementById("recipe-container");
+  container.innerHTML = ""; 
+
+  bebidas.forEach(bebida => {
+    const bebidaCard = document.createElement("div");
+    bebidaCard.classList.add("recipe-card");
+
+    bebidaCard.innerHTML = `
+      <img src="${bebida.image}" alt="${bebida.title}">
+      <h3>${bebida.title}</h3>
+      <a href="detalle.html?id=${bebida.id}">Ver receta completa</a>
+    `;
+
+    container.appendChild(bebidaCard);
+  });
+}
+
